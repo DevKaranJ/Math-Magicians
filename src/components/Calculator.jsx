368a7884672a52712514bc6d8807c664d919b549
Karan Joshi <Karan190213@gmail.com>
2023-09-21 16:58:03 +0530

    Implemented Calculator Component Logic
    In this commit, I have added the logic needed to make the Calculator component functional. I imported the necessary math logic from calculate.js and operate.js into the Calculator.js component.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,40 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import '../styles/Calculator.css';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [calculatorData, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (buttonName) => {
+    const newCalculatorData = calculate(calculatorData, buttonName);
+    setCalculatorData(newCalculatorData);
+  };
+
   return (
     <div className="calculator">
-      <Display className="input-display" />
+      <Display className="input-display" value={calculatorData.next || calculatorData.total || '0'} />
       <div className="calculator-buttons">
         <div className="calculator-row">
-          <button type="submit" className="calculator-button">AC</button>
-          <button type="submit" className="calculator-button">+/-</button>
-          <button type="submit" className="calculator-button">%</button>
-          <button type="submit" className="calculator-button">÷</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('AC')}>AC</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('+/-')}>+/-</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('%')}>%</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('÷')}>÷</button>
         </div>
         <div className="calculator-row">
-          <button type="submit" className="calculator-button">7</button>
-          <button type="submit" className="calculator-button">8</button>
-          <button type="submit" className="calculator-button">9</button>
-          <button type="submit" className="calculator-button">x</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('7')}>7</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('8')}>8</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('9')}>9</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('x')}>x</button>
         </div>
         <div className="calculator-row">
-          <button type="submit" className="calculator-button">4</button>
-          <button type="submit" className="calculator-button">5</button>
-          <button type="submit" className="calculator-button">6</button>
-          <button type="submit" className="calculator-button">-</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('4')}>4</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('5')}>5</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('6')}>6</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('-')}>-</button>
         </div>
         <div className="calculator-row">
-          <button type="submit" className="calculator-button">1</button>
-          <button type="submit" className="calculator-button">2</button>
-          <button type="submit" className="calculator-button">3</button>
-          <button type="submit" className="calculator-button">+</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('1')}>1</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('2')}>2</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('3')}>3</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('+')}>+</button>
         </div>
         <div className="calculator-row">
-          <button type="submit" className="calculator-button">0</button>
-          <button type="submit" className="calculator-button">.</button>
-          <button type="submit" className="calculator-button">=</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('0')}>0</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('.')}>.</button>
+          <button type="button" className="calculator-button" onClick={() => handleButtonClick('=')}>=</button>
         </div>
       </div>
     </div>
