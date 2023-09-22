6430468f6c0e17e87fa202de31862103bbc5f7b1
Karan Joshi <Karan190213@gmail.com>
2023-09-22 19:57:14 +0530

    Add Quote Display Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <div className="Quote">
+        <Quote />
+      </div>
     </div>
   );
 }
