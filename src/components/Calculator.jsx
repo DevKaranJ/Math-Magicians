a659f560ff22f4674a8785d6e3bb3df41ed34f35
Karan Joshi <Karan190213@gmail.com>
2023-09-28 13:14:15 +0530

    Calculator page ui design update

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -16,40 +16,43 @@ function Calculator() {
   };
 
   return (
-    <div className="calculator">
-      <div className="calculator-heading">
-        <h1>Welcome to Math Magicians</h1>
-      </div>
-      <Display className="input-display" value={calculatorData.next || calculatorData.total || '0'} />
-      <div className="calculator-buttons">
-        <div className="calculator-row">
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('AC')}>AC</button>
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('+/-')}>+/-</button>
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('%')}>%</button>
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('÷')}>÷</button>
+    <div className="calculator-ui">
+      <div className="calculator">
+        <div className="calculator-heading">
+          <h1>Let&apos;s do some math!</h1>
+
         </div>
-        <div className="calculator-row">
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('7')}>7</button>
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('8')}>8</button>
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('9')}>9</button>
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('x')}>x</button>
-        </div>
-        <div className="calculator-row">
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('4')}>4</button>
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('5')}>5</button>
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('6')}>6</button>
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('-')}>-</button>
-        </div>
-        <div className="calculator-row">
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('1')}>1</button>
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('2')}>2</button>
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('3')}>3</button>
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('+')}>+</button>
-        </div>
-        <div className="calculator-row">
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('0')}>0</button>
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('.')}>.</button>
-          <button type="button" className="calculator-button" onClick={() => handleButtonClick('=')}>=</button>
+        <Display className="input-display" value={calculatorData.next || calculatorData.total || '0'} />
+        <div className="calculator-buttons">
+          <div className="calculator-row">
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('AC')}>AC</button>
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('+/-')}>+/-</button>
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('%')}>%</button>
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('÷')}>÷</button>
+          </div>
+          <div className="calculator-row">
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('7')}>7</button>
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('8')}>8</button>
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('9')}>9</button>
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('x')}>x</button>
+          </div>
+          <div className="calculator-row">
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('4')}>4</button>
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('5')}>5</button>
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('6')}>6</button>
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('-')}>-</button>
+          </div>
+          <div className="calculator-row">
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('1')}>1</button>
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('2')}>2</button>
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('3')}>3</button>
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('+')}>+</button>
+          </div>
+          <div className="calculator-row">
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('0')}>0</button>
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('.')}>.</button>
+            <button type="button" className="calculator-button" onClick={() => handleButtonClick('=')}>=</button>
+          </div>
         </div>
       </div>
     </div>
